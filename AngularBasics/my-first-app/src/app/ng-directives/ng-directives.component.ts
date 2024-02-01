2b9ee4a26d3588d2ae9760d54229db76a4f129c6
Abhi <abhi17patel@gmail.com>
2024-01-31 22:59:51 -0500

    ng-directives

--- AngularBasics/my-first-app/src/app/ng-directives/ng-directives.component.ts
+++ AngularBasics/my-first-app/src/app/ng-directives/ng-directives.component.ts
@@ -11,10 +11,14 @@ export class NgDirectivesComponent implements OnInit {
   serverCreationStatus = "No Server was created!";
   serverCreated = false;
   serverName = 'ABCSERVER';
+
+  servers = ["TestServer1", "Ts2", "TS3"];
   constructor() { 
     setTimeout(()=>{
       this.allowNewServer = true;
-    },2000)
+    },2000);
+
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
 
   ngOnInit(): void {
@@ -22,8 +26,22 @@ export class NgDirectivesComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated=true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was creted!";
   }
 
+  //ng-Style code
+
+  serverId: number =10;
+  //autoamtically pick type
+  serverStatus = 'offline';
+
+  getServerStatus(){
+      return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 
 }
