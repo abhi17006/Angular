34b7dc3bed54c2c70cba72a700a0bdfa5480ed32
Abhi <abhi17patel@gmail.com>
2024-02-02 14:11:16 -0500

    add recipe-list component, recipe detail component

--- Angular-Project/shopping-list-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-Project/shopping-list-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  //array of Recipe type class
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe', 'Recipe description', 'https://www.nehascookbook.com/wp-content/uploads/2022/11/Paneer-tikka-masala-WS.jpg'),
+    new Recipe('A Test Recipe', 'Recipe description', 'https://www.nehascookbook.com/wp-content/uploads/2022/11/Paneer-tikka-masala-WS.jpg'
+    )
+  ];
+  constructor(){
 
+  }
+  ngOnInit(){}
 }
