53bc2d160701d8bf579c4a157633c28ed65fd9e2
Abhi <abhi17patel@gmail.com>
2024-02-03 18:19:00 -0500

    Create custom event in parent component ans pass its values to child componet using EventEMitter, call emit method

--- AngularBasics/database-binding-component/src/app/app.component.ts
+++ AngularBasics/database-binding-component/src/app/app.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-
+  // passing servers details into Frontend
   serverElements = [{type: 'server', name:'Testserver', content:'Just a test!'}]
   constructor(){}
   ngOnInit(){
-    
+  }
+
+  //expect event data from HTML APp component
+  onServerAdded(serverData: {serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
   }
 }
