1ac70d0f553949862df18fdb1e3961fea8470051
Abhi <abhi17patel@gmail.com>
2024-02-02 22:45:57 -0500

    create model class, add Array of elements and run for loop in HTML shopping list

--- Angular-Project/shopping-list-app/src/app/shoping-list/shoping-list.component.ts
+++ Angular-Project/shopping-list-app/src/app/shoping-list/shoping-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
   styleUrl: './shoping-list.component.css'
 })
-export class ShopingListComponent {
+export class ShopingListComponent implements OnInit{
+
+  ingredients : Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tometoes', 15),
+    new Ingredient('Banana', 2.5)
+  ];
+  constructor(){
+
+  }
+  ngOnInit(){
+   
+  }
 
 }
